2ca07de80b8a88e9b7ad06775067f59868217615
huynguyen411 <56786229+huynguyen411@users.noreply.github.com>
2020-10-18 22:17:11 +0700

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-
-public class Main {
-    public static void main(String[] args) {
-        DictionaryManagement dictionaryManagement = new DictionaryManagement();
-        dictionaryManagement.dictionaryBasic();
-    }
-}
